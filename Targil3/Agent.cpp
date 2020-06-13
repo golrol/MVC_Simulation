688218df420a686e57665134f1628f940b55508f
Gal R <galrn6@gmail.com>
2020-06-13 21:56:50 +0300

    deleted comments

--- Targil3/Agent.cpp
+++ Targil3/Agent.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Admin on 13/06/2020.
-//
-
 #include "Agent.h"
--- Targil3/Agent.h
+++ Targil3/Agent.h
@@ -1,7 +1,3 @@
-//
-// Created by Admin on 13/06/2020.
-//
-
 #ifndef AOOP_TARGIL3_AGENT_H
 #define AOOP_TARGIL3_AGENT_H
 
