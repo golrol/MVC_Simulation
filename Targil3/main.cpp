8c4358e18ac3de88d5378f28298cca30e81e8e78
Gal R <galrn6@gmail.com>
2020-06-13 20:37:10 +0300

    init

---
+++ Targil3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Targil3
+//
+//  Created by Gal Raykin on 13/06/2020.
+//  Copyright © 2020 Gal Raykin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
