79112f6ba450442f24acfb736131fa0dcd97e069
Yuval Zel-zion <yuvalzelzion@gmail.com>
2020-06-21 18:42:21 +0300

    added skeletonController.cpp

--- Targil3/Validation.cpp
+++ Targil3/Validation.cpp
@@ -112,62 +112,6 @@ bool commandValidation(string strLine){
     while (getline(ssLine, token, ' ')) {
         commandVec.push_back(token);/*put the string in the vector*/
     }
-    if(commandVec.empty()){
-        if(commandVec[0] == "default"){
-            if(commandVec.size()!=1){
-                //TODO: throw exception (to many arguments)
-            }
-        }
-        else if(commandVec[0] == "size"){
-            if(commandVec.size()!=2)
-                //TODO: throw exception (to many arguments)
-            try {
-                int x = intValidation(commandVec[1])
-            }
-            catch (...) {
-
-            }
-        }
-        else if(commandVec[0] == "zoom"){
-
-        }
-        else if(commandVec[0] == "pen"){
-
-        }
-        else if(commandVec[0] == "show"){
-
-        }
-        else if(commandVec[0] == "create"){
-
-        }
-        else if(true){//TODO: check if commandVec[0} is a name of existing agent
-            if(commandVec[1] == "course"){
-
-            }
-            else if(commandVec[1] == "position"){
-
-            }
-            else if(commandVec[1] == "destination"){
-
-            }
-            else if(commandVec[1] == "position") {
-
-            }
-            else if(commandVec[1] == "stop") {
-
-            }
-            else if(commandVec[1] == "attack") {
-
-            }
-        }
-
-    }
-    else {/*if commandVec is empty*/
-        //TODO: throw exception (no command)
-    }
-
-
-
     }
 
 
