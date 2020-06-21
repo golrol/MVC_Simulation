977d0ae3f25f35243d866003dc613233b2c165c0
Yuval Zel-zion <yuvalzelzion@gmail.com>
2020-06-21 20:19:50 +0300

    continue work on command validation

--- Targil3/Validation.cpp
+++ Targil3/Validation.cpp
@@ -189,6 +189,7 @@ bool showValidation(const string& strLine){
     }
     else
         return true;
+    return false;
 }
 bool statusValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
@@ -197,34 +198,40 @@ bool statusValidation(const string& strLine){
     }
     else
         return true;
+    return false;
 }
 bool goValidation(const string& strLine){
-    vector<string> vec(tokenizeLine(strLine));
     vector<string> vec(tokenizeLine(strLine));
     if(vec.size()!=1){
         //TODO: throw exception (wrong number of arguments)
     }
     else
         return true;
+    return false;
 }
 bool createValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
-
+    return false;
 }
 pair<Point,int> courseValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
+    return make_pair(Point(),0);
 }
 pair<Point,int> positionValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
+    return make_pair(Point(),0);
 }
 string destinationValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
+    return "";
 }
 bool stopValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
+    return false;
 }
 string attackValidation(const string& strLine){
     vector<string> vec(tokenizeLine(strLine));
+    return "";
 }
 
 
